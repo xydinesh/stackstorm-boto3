099c7ce814ac7a88efa7537423700a96fea5ff13
“Dinesh <xydinesh@gmail.com>
2017-04-12 21:25:02 +0000

    Rename kwargs as params

--- actions/action.py
+++ actions/action.py
@@ -5,10 +5,10 @@ from st2actions.runners.pythonrunner import Action
 
 # pylint: disable=too-few-public-methods
 class Boto3ActionRunner(Action):
-    def run(self, service, region, action_name, kwargs):
+    def run(self, service, region, action_name, params):
         client = boto3.client(service, region_name=region)
-        if kwargs is not None:
-            response = getattr(client, action_name)(**kwargs)
+        if params is not None:
+            response = getattr(client, action_name)(**params)
         else:
             response = getattr(client, action_name)()
         return (True, response)
