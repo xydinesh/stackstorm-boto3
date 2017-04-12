47f0202bc61920ccebaac9ae600899b2cece55f2
Dinesh Weerapurage <xydinesh@gmail.com>
2017-04-12 04:18:30 +0000

    Implemented boto3 arbitary action execution.

--- actions/action.py
+++ actions/action.py
@@ -5,6 +5,10 @@ from st2actions.runners.pythonrunner import Action
 
 # pylint: disable=too-few-public-methods
 class Boto3ActionRunner(Action):
-    def run(self, service, action, kwargs):
-        print service, action, kwargs
-        return (True, "")
+    def run(self, service, region, action_name, kwargs):
+        client = boto3.client(service, region_name=region)
+        if kwargs is not None:
+            response = getattr(client, action_name)(**kwargs)
+        else:
+            response = getattr(client, action_name)()
+        return (True, response)
